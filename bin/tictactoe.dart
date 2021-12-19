c73a6eea08bb6cee9b74bf65b349399074ea5387
Fuad Syafaat Muharram <fsyafaatm@gmail.com>
2021-12-19 14:24:18 +0800

    Add generate drawboard function

--- bin/tictactoe.dart
+++ bin/tictactoe.dart
@@ -10,4 +10,34 @@ void main() {
 
 void drawBoard(int squareSize) {
   // Selamat ngoding :)
+  // Selamat ngoding :)
+
+  List<dynamic> listGrid = [];
+
+  for (int row = 0; row <= squareSize * 2; row++) {
+    List<dynamic> listTemp = [];
+    if (row % 2 == 1) {
+      for (int a = 0; a <= squareSize; a++) {
+        listTemp.add('|');
+        listTemp.add('   ');
+      }
+    } else {
+      for (int b = 0; b <= squareSize; b++) {
+        if (b >= squareSize) {
+          listTemp.add('');
+        } else {
+          listTemp.add(' ');
+          listTemp.add('---');
+        }
+      }
+    }
+    listGrid.add(listTemp);
+  }
+
+  for (int row = 0; row < listGrid.length; row++) {
+    for (int c = 0; c < listGrid[0].length; c++) {
+      stdout.write(listGrid[row][c]);
+    }
+    print('');
+  }
 }
